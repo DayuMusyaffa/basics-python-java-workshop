a95b43bdc28b364b267df5d8d953ddf630052fe7
unknown <muhammaddayu.musyaffa@cakrawala.ac.id>
2025-10-29 12:12:23 +0700

    Soal 1 - 5 Clear

--- python/challenges/4_function_is_even_factorial.py
+++ python/challenges/4_function_is_even_factorial.py
@@ -1,5 +1,10 @@
 # TODO: Fungsi untuk memeriksa apakah sebuah bilangan genap
 def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
     pass
 
 # Tes fungsi
@@ -8,6 +13,12 @@ print(is_even(7))   # False
 
 # TODO: Fungsi untuk menghitung faktorial
 def factorial(n):
+    a = 1
+    hasil = 1
+    while a <= n:
+        hasil *= a
+        a += 1
+    return hasil
     pass
 
 # Tes fungsi
