cd25a66f972310a57986f08067ccfcac07e22205
unknown <muhammaddayu.musyaffa@cakrawala.ac.id>
2025-10-29 12:45:42 +0700

    Soal 3 Clear

--- python/challenges/3_loop_odd_vocal.py
+++ python/challenges/3_loop_odd_vocal.py
@@ -1,11 +1,14 @@
 # TODO: Program untuk mencetak bilangan ganjil dari 1 hingga 15
-odd_numbers = [x for x in range(1, 16) if x 2% == 1]
-print(odd_numbers)
+odd_numbers = [x for x in range(1, 16) if x % 2 == 1]
+print("Bilangan ganjil 1 sampai 15: ", odd_numbers)
 
 # TODO: Program menghitung jumlah huruf vokal
 word = input("Masukkan kata: ").lower()
 
 vowels = "aiueo"
 count = 0
+for letter in word:
+    if letter in vowels:
+        count += 1
 
 print("Jumlah huruf vokal:", count)
