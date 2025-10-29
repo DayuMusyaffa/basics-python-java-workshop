759cf6652e49556f8b497a6e6a3c1d45f2becdac
unknown <muhammaddayu.musyaffa@cakrawala.ac.id>
2025-10-29 11:08:18 +0700

    Soal 2 Clear

--- python/challenges/2_condition_age.py
+++ python/challenges/2_condition_age.py
@@ -1,5 +1,17 @@
 # Program Klasifikasi Usia
 
 # 1. TODO: Baca input dari pengguna
+umur = int(input("masukkan umur anda: "))
 
 # 2. TODO Tentukan kategori berdasarkan usia
+kategori_usia = umur
+if umur >= 60:
+    print("Lansia")
+elif umur >= 18:
+    print("Dewasa")
+elif umur >= 12:
+    print("Remaja")
+else:
+    print("Anak")
+
+print("Anda Berumur: ", kategori_usia)
